b4285a66961285077266413b509ba183a8642e3c
livelivelive007 <jeremylive096@gmail.com>
2020-11-29 12:17:39 -0600

    update1

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import math 
-from math import pi
+from math import pi,sin,cos
 import random
 
-#print(int('01010', 2))    
 
 """ cromosomas de las abejas:
 direccion favorita (norte, noreste, etc)
@@ -19,22 +18,40 @@ cromosomas de las flores:
  posicion
 """
 
-#oeste,este,norte,sur
-angulos_direcciones = [0, pi*5/6, pi*3/4, pi*2/3, pi/2, pi/3, pi/4,
-                       pi/6, pi, -pi/6, -pi/4, -pi/3, -pi/2, -pi*2/3, -pi*3/4, -pi*5/6]
-largo_angulos_posibles = len(angulos_direcciones)-1
-inicial_random = random.randint(0, largo_angulos_posibles)
+"""
+def ladoEscogido(pLenAngulo: int):
+        if pLenAngulo == 0 or pLenAngulo == 1 or pLenAngulo == 7:
+            return 1
+        if pLenAngulo == 3 or pLenAngulo == 4 or pLenAngulo == 5:
+            return 0
 
+def distanciaMax():
+
+    return 0
+"""
+#oeste,este,norte,sur
+angulos_direcciones = [0, pi*3/4, pi/2, pi/4, pi, -pi/4, -pi/2, -pi*3/4]
+largo_angulos_posibles = len(angulos_direcciones)-1
+inicial_random_indice = random.randint(0, largo_angulos_posibles)
+inicial_random = angulos_direcciones[inicial_random_indice]
+
+#lado_escogido = ladoEscogido(inicial_random_indice) #A cual lado debo mandar el rango si a la der o izq para encontrar la distanciaMaxima
+
+"""
+Generic Logic
+"""
 class Abeja:
     def __init__(self,padre=None,madre=None):
         if padre==None or madre==None:
-            desviacionMaxima=random
+            desviacionMaxima=0
             direccionFavorita=inicial_random
             colorFavorito=(0,0,255)
             toleranciaAlColor=0
-            anguloDesviacion=pi
+            anguloDesviacion=0
             recorrido=[(200,200),2] #2=random
             nectar_recolectado=[]
+            #distanciaMaxima=distanciaMax(lado_escogido)
+            distanciaMaxima=0
             self.desviacionMaxima=desviacionMaxima
             self.direccionFavorita=direccionFavorita
             self.colorFavorito=colorFavorito
@@ -42,25 +59,41 @@ class Abeja:
             self.anguloDesviacion=anguloDesviacion
             self.recorrido=recorrido
             self.nectar_recolectado=nectar_recolectado
+            self.distanciaMaxima=distanciaMaxima
         else:
-            reproducir a papá y mamá XD
+            return 0
+            #reproducir a papá y mamá XD
     def cruce(self,otraAbeja):
-        
-    def simularRecorrido(self,)
+        return 0
+    def simularRecorrido(self):
+        #para el recorrido random
+        distanciaDesdeElCentro=random()*self.distanciaMaxima
+        angulo=random.randint(self.direccionFavorita-self.desviacionMaxima,self.direccionFavorita+self.desviacionMaxima)
+        x=50+sin(angulo)*distanciaDesdeElCentro
+        y=50+cos(angulo)*distanciaDesdeElCentro
 
+
+        #para anchura:
+        #for r in range(0,self.dis)
+        #return 0
+"""
+Garden Logic
+"""
 class Flor:
     def __init__(self,pRadio,pAngulo,pMuestras):
         self.radio = pRadio
         self.anagulo = pAngulo
         self.muestras = pMuestras
 
-## Papa
-papa=Abeja(None,None)
 
-## MAMA
-mama=Abeja(None,None)
+"""
+Setup
+"""
+ancho, alto = 100
 
-## 2 Hijos
 
-Abeja(papa,mama)
-Abeja(papa,mama)
+
+"""
+Pasar de un binario a entero.
+print(int('01010', 2))
+"""
